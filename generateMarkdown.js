32580aee659b35a817e7f5b76eb6b9ddbf9ca532
Anthony Jimenez <tony6776a@gmail.com>
2022-01-18 13:02:14 -0500

    started added the generateMarkdown function

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -23,15 +23,21 @@ function renderLicenseLink(license) {
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-  function renderLicenseSection(license) {
-    return `# License
-    ## The Licenses We Used [${license}](${renderLicenseLink(license)})`
-    }
+function renderLicenseSection(license) {
+  return `# License
+  ## The Licenses We Used [${license}](${renderLicenseLink(license)})`
+  }
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
-}
+[![License Badge](${renderLicenseBadge(data.license)})](${renderLicenseLink(data.license)})
+  ## Table of Contents
+  - [Description](#Description)
+  - [Installations](#Installations)
+  - [Usage](#Usage)
+  - [Tests](#Tests)
+  - [License](#License)
+  - [Questions](#Questions)
+  
 
 module.exports = generateMarkdown;
